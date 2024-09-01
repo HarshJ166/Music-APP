6fa7db0141d5ad7dd3252793024aa3ec8fd46477
unknown <harsh.jajal16606@sakec.ac.in>
2024-09-01 14:48:01 +0530

    third

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,8 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET??""
+          clientId: "1084828078888-55555555555555555555555555555555.apps.googleusercontent.com",
+          clientSecret: "GOCSPX-12345678901234567890123456789012"
         })
       ]
 })
